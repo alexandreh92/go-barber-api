cf34f0fe0dc7ec5e50d800d7d2dbacbb6b3af76f
Alexandre Hansen <eng.alexandreh@gmail.com>
2020-03-10 18:33:56 -0300

    feat(global): added gems and Rails Routes configs
    Changed application_controller to respond with public/index.html which is react app in front end and
    added postgres, devise, devise-jwt and faker gems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
+
+  def fallback_index_html
+    render file: 'public/index.html'
+  end
+
+  def render_resource(resource)
+    if resource.errors.empty?
+      render json: resource
+    else
+      validation_error(resource)
+    end
+  end
+
+  def validation_error(resource)
+    render json: {
+      errors: [
+        {
+          status: '400',
+          title: 'Bad Request',
+          detail: resource.errors,
+          code: '100'
+        }
+      ]
+    }, status: :bad_request
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # routes here
+
+  get '*path', to: 'application#fallback_index_html', constraints: lambda { |request|
+    !request.xhr? && request.format.html?
+  }
 end
