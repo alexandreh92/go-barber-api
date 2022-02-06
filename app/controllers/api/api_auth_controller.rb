e4abf4aeb54082d6ecd63061b6eec48b793ec83d
Alexandre Stapenhorst <eng.alexandreh@gmail.com>
2022-02-05 21:13:42 -0300

    fix: fix swagger

--- app/controllers/api/api_auth_controller.rb
+++ app/controllers/api/api_auth_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Api
   class ApiAuthController < ApiController
     skip_before_action :authenticate_user!
--- config/initializers/rswag-ui.rb
+++ config/initializers/rswag-ui.rb
@@ -1,4 +1,4 @@
-if Rails.env.development? Rails.env.test?
+if Rails.env.development? || Rails.env.test?
   Rswag::Ui.configure do |c|
     # List the Swagger endpoints that you want to be documented through the swagger-ui
     # The first parameter is the path (absolute or relative to the UI host) to the corresponding
@@ -6,7 +6,7 @@ if Rails.env.development? Rails.env.test?
     # NOTE: If you're using rspec-api to expose Swagger files (under swagger_root) as JSON or YAML endpoints,
     # then the list below should correspond to the relative paths for those endpoints
 
-    c.swagger_endpoint '/api-docs/v1/swagger.yaml', 'API V1 Docs'
+    c.swagger_endpoint '/swagger/v1/swagger.yaml', 'GoBarber V1'
 
     # Add Basic Auth in case your API is private
     # c.basic_auth_enabled = true
